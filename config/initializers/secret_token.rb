0ef2783f9e1a94d52e9b15d908a4829496aca31c
Ozzie Gooen <Ozzie@BowLabs.org>
2013-05-04 16:13:43 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tpp::Application.config.secret_token = '3bf3ea66949ec77527e5956387b1aa8431e5b974ee62e3bfab36a086b4253413965aecd4c9cb71c63467d647a48c518b2962c296dcfe7f6041d43b0038826af6'
